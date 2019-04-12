8cb88968bf22fbb4c9e14627922ac9ee1d82b3af
Ho Kim <besqer996@gnu.ac.kr>
2019-04-12 22:27:49 +0900

    Add frame SignUpWidget

--- lib/widget/common/alert.dart
+++ lib/widget/common/alert.dart
@@ -2,21 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:questrip/lib.dart';
 import 'package:questrip/net/lib.dart';
-import 'package:questrip/resources.dart';
+import 'package:questrip/res/lib.dart';
 
 /// 알림창, Toast 팝업 다이얼로그 등의 기능을 총괄합니다.
 /// 
 /// 담당자: 김호
 
 /// 알림창을 띄웁니다.
-void dialog(final context, final String msgId, { Runnable onConfirm, Runnable onCancel }) {
+void dialog(final BuildContext context, final String msgId,
+    { Runnable onConfirm, Runnable onCancel }) {
   onCancel == null
       ? _dialogSimple(context, msgId, onConfirm: onConfirm)
       : _dialogAsk(context, msgId, onConfirm: onConfirm, onCancel: onCancel);
 }
 
 /// 사용자에게 오류를 알림창을 통해 알립니다.
-void dialogFailed(final context, final Failed failed,
+void dialogFailed(final BuildContext context, final Failed failed,
     { Runnable onConfirm, final String rejected, final String canceled }) {
   String msgId;
   switch (failed) {
@@ -39,6 +40,12 @@ void dialogFailed(final context, final Failed failed,
   _dialogSimple(context, msgId, onConfirm: onConfirm);
 }
 
+/// 사용자에게 오류를 알림창을 통해 알립니다.
+Future<bool> dialogExit(final BuildContext context) async {
+  _dialogAsk(context, R.string.common_alert_field_close_app, onConfirm: exit);
+  return false;
+}
+
 /// Toast 메세지를 띄웁니다.
 void toast(String msgId) {
   Fluttertoast.showToast(
@@ -49,52 +56,54 @@ void toast(String msgId) {
 }
 
 /// 확인 버튼 하나있는 간단한 알림창을 띄웁니다.
-void _dialogSimple(final context, final String msg, { Runnable onConfirm }) async {
+void _dialogSimple(final BuildContext context, final String msg,
+    { Runnable onConfirm }) async {
   showDialog(context: context,
     barrierDismissible: onConfirm == null,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(R.string.common_alert_title),
-          content: Text(msg),
-          actions: <Widget>[
-            FlatButton(
-                child: Text(R.string.common_alert_confirm),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  if (onConfirm != null) onConfirm();
-                }
-            ),
-          ],
-        );
-      },
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(R.string.common_alert_title),
+        content: Text(msg),
+        actions: <Widget>[
+          FlatButton(
+              child: Text(R.string.common_alert_confirm),
+              onPressed: () {
+                Navigator.of(context).pop();
+                if (onConfirm != null) onConfirm();
+              }
+          ),
+        ],
+      );
+    },
   );
 }
 
 /// 예, 아니오 버튼이 있는 알림창을 띄웁니다.
-void _dialogAsk(final context, final String msg, { Runnable onConfirm, Runnable onCancel }) {
+void _dialogAsk(final BuildContext context, final String msg,
+    { Runnable onConfirm, Runnable onCancel }) {
   showDialog(context: context,
     barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(R.string.common_alert_title),
-          content: Text(msg),
-          actions: <Widget>[
-            FlatButton(
-                child: Text(R.string.common_alert_choose_yes),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  if (onConfirm != null) onConfirm();
-                }
-            ),
-            FlatButton(
-                child: Text(R.string.common_alert_choose_no),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  if (onCancel != null) onCancel();
-                }
-            ),
-          ],
-        );
-      },
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(R.string.common_alert_title),
+        content: Text(msg),
+        actions: <Widget>[
+          FlatButton(
+              child: Text(R.string.common_alert_choose_yes),
+              onPressed: () {
+                Navigator.of(context).pop();
+                if (onConfirm != null) onConfirm();
+              }
+          ),
+          FlatButton(
+              child: Text(R.string.common_alert_choose_no),
+              onPressed: () {
+                Navigator.of(context).pop();
+                if (onCancel != null) onCancel();
+              }
+          ),
+        ],
+      );
+    },
   );
 }
